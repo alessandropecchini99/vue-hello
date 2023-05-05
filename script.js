ec42afe0b7f8c68a3111120c80d8393aabff4d34
alessandropecchini99 <pecchini99@gmail.com>
2023-05-05 20:27:53 +0200

    finish es

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log(Vue)
 
-Vue.createApp({
-    
+
+// UN TIPO DI SINTASSI --------------------
+// const { createApp, altri_metodi } = Vue;
+// createApp({
+// }).mount(`#app`);
+
+// PROVA 1
+const app = Vue.createApp({
+    data: function() {
+        return {
+            message: `Hello!`
+        }
+    }
 });
+
+app.mount(`#app`);
